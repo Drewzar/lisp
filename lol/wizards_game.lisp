b4da4785aeda08d25d84694bfeeab1fe1aa52a87
Drew Oliner <drew@hackinggibsons.com>
2011-05-08 20:11:14 -0400

    comments are good

--- lol/wizards_game.lisp
+++ lol/wizards_game.lisp
@@ -1,6 +1,6 @@
 ;; wizards game
 
-;;define the 3 places (*nodes*) where the games takes palce.
+;; define the 3 places (*nodes*) where the games takes palce.
 (defparameter *nodes* '((living-room (you are in the living-room. 
 				      a wizard is snoring loudly on the couch.))
 			(garden (you are in a beautiful garden.
@@ -8,5 +8,6 @@
 			(attic (you are in the attic.
 				there is a giant welding torch in the corner.))))
 
+;; define a function to describe the location.
 (defun describe-location (location node)
   (cadr (assoc location node)))
