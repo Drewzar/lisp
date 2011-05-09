74f7ed0c4a6c2bcee5fc3f9e2bf2995d8ccba73b
Drew Oliner <drew@hackinggibsons.com>
2011-05-08 22:46:44 -0400

    we can now look around.

--- lol/wizards_game.lisp
+++ lol/wizards_game.lisp
@@ -46,3 +46,12 @@
   (labels ((describe-obj (obj)
 	     `(you see a ,obj on the floor.)))
     (apply #'append (mapcar #'describe-obj (objects-at loc objs obj-loc)))))
+
+;; define players location
+(defparameter *location* 'living-room)
+
+;; define a look function
+(defun look ()
+  (append (describe-location *location* *nodes*)
+	  (describe-paths *location* *edges*)
+	  (describe-objects *location* *objects* *object-locations*)))
