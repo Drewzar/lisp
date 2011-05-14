78b54fc8903dbc97d6aac0c5999d02646462dc77
Drew Oliner <drew@hackinggibsons.com>
2011-05-14 05:59:00 -0400

    Now have a working repl

--- lol/wizards_game.lisp
+++ lol/wizards_game.lisp
@@ -78,4 +78,47 @@
 (defun inventory ()
   (cons 'items-  (objects-at 'body *objects* *object-locations*)))
 
+;; define a repl for the game
+(defun game-repl ()
+  (let ((cmd (game-read)))
+    (unless (eq (car cmd) 'quit)
+      (game-print (game-eval cmd))
+      (game-repl))))
 
+;; define a custom (read) for our repl
+(defun game-read ()
+  (let ((cmd (read-from-string
+	      (concatenate 'string "(" (read-line) ")"))))
+    (flet ((quote-it (x)
+	     (list 'quote x)))
+      (cons (car cmd) (mapcar #'quote-it (cdr cmd))))))
+
+;; a list of allowed commands
+(defparameter *allowed-commands* '(look walk pickup inventory))
+
+;; define a custom (eval) for our repl
+(defun game-eval (sexp)
+  (if (member (car sexp) *allowed-commands*)
+      (eval sexp)
+    '(i do not know that command.)))
+
+;; define a function to tweak text
+(defun tweak-text (lst caps lit)
+  (when lst
+    (let ((item (car lst))
+	  (rest (cdr lst)))
+      (cond ((eq item #\space) (cons item (tweak-text rest caps lit)))
+	    ((member item '(#\! #\? #\.)) (cons item (tweak-text rest t lit)))
+	    (lit (cons item (tweak-text rest nil lit)))
+	    ((or caps lit) (cons (char-upcase item) (tweak-text rest nil lit)))
+	    (t (cons (char-downcase item) (tweak-text rest nil nil)))))))
+
+;; define a custom (print) for our repl
+(defun game-print (lst)
+  (princ (coerce (tweak-text (coerce (string-trim "() "
+						  (prin1-to-string lst))
+				     'list)
+			     t
+			     nil)
+		 'string))
+  (fresh-line))
